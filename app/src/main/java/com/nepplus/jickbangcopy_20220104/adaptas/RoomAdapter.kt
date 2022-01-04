a6310b625c8ad17b039f400c51b75fd9a2152045
김현희 <tta_bi@naver.com>
2022-01-04 15:00:54 +0900

    RoomAdapter 클래스 제작 중 - inner class 생성 / 에러 해결 - 받아올 변수 생성해서 넣어줌

---
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/adaptas/RoomAdapter.kt
@@ -0,0 +1,11 @@
+package com.nepplus.jickbangcopy_20220104.adaptas
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RoomAdapter : RecyclerView.Adapter<>() {
+
+    inner class MyViewHolder(row : View) : RecyclerView.ViewHolder(row) {
+
+    }
+}
