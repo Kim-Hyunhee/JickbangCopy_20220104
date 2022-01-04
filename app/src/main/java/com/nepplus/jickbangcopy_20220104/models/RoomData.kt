a02d804f26bed7272eae4dc9cb103501ada5f7cd
김현희 <tta_bi@naver.com>
2022-01-04 15:39:57 +0900

    층수를 가공해주는 함수(Python : 메쏘드) 추가

--- app/src/main/java/com/nepplus/jickbangcopy_20220104/models/RoomData.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220104/models/RoomData.kt
@@ -7,4 +7,18 @@ class RoomData(
     val floor : Int,
     val description : String,  // 파이썬과 같이 추가로 들어올 파라미터가 없어도 ,로 마무리해도 됨
 ) {
+
+    fun getFormattedFloor() : String{
+
+        if (this.floor > 0) {
+            return "${ this.floor }층"  // 2층, 3층...
+        }
+        else if (this.floor < 0) {
+            return "지하 ${-this.floor}층"  // 지하 1층
+        }
+        else {
+            return "반지하"  // 양수도 음수도 아닌 건 0 밖에 없다.
+        }
+
+    }
 }
